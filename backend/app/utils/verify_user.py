124254902ec6b950293ac78e574de0ec983bcf02
zici_fish <fwioe77@163.com>
2026-09-10 15:09:22 +0800

    修复了验证报错

--- backend/app/utils/verify_user.py
+++ backend/app/utils/verify_user.py
@@ -19,14 +19,15 @@ def verify_login(credentials: Optional[HTTPAuthorizationCredentials]) -> int:
 
 
 def verify_admin(credentials: Optional[HTTPAuthorizationCredentials]) -> bool:
+    """验证管理员身份，是返回 True，否返回 False，缺 token 抛 401。"""
     if not credentials:
         raise HTTPException(status_code=401, detail="未提供凭证")
     token = credentials.credentials
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        is_admin = payload.get('is_admin', False)
+        is_admin = payload.get('is_admin', 0)
         if not is_admin:
-            raise False #这里不能直接raise
+            return False
         return True
     except JWTError:
         raise HTTPException(401, "token无效")
